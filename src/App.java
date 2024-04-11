df490d6545b7e6d00a1da0acb771e041c8fc771e
Alfredo Silva <a.alfredops@gmail.com>
2024-04-11 09:03:58 -0400

    Initial Strucutre + LinkedList

---
+++ src/App.java
@@ -0,0 +1,7 @@
+import LinkedList.LinkedList;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        LinkedList myLinkedList = new LinkedList(10);
+    }
+}
---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,25 @@
+package LinkedList;
+
+public class LinkedList {
+    private Node head;
+    private Node tail;
+    private int length;
+
+    class Node{
+        int value;
+        Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+
+}
