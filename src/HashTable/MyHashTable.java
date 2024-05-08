bab3218580710a3b3add54ea5a94c4e6676e4a50
Alfredo Silva <a.alfredops@gmail.com>
2024-05-08 11:49:14 -0400

    HashTable theory and Exercises

--- src/HashTable/MyHashTable.java
+++ src/HashTable/MyHashTable.java
@@ -2,44 +2,23 @@ package HashTable;
 
 import java.util.ArrayList;
 
-public class HashTable {
+public class MyHashTable {
     private int size = 7;
     private Node[] dataMap;
 
     public class Node {
-        private String key;
-        private int value;
-        private Node next;
+        String key;
+        int value;
+        Node next;
 
         public Node(String key, int value) {
-            this.key = key;
             this.value = value;
+            this.key = key;
         }
     }
 
-    public HashTable() {
+    public MyHashTable() {
         dataMap = new Node[size];
-    }
-
-    public void printTable() {
-        for (int i = 0; i < dataMap.length; i++) {
-            System.out.println(i + ":");
-            Node temp = dataMap[i];
-            while (temp != null) {
-                System.out.println(" {" + temp.key + " = " + temp.value + " }");
-                temp = temp.next;
-            }
-        }
-    }
-
-    private int hash(String key) {
-        int hash = 0;
-        char[] keyChars = key.toCharArray();
-        for (int i = 0; i < keyChars.length; i++) {
-            int asciiValue = keyChars[i];
-            hash = (hash + asciiValue * 23) % dataMap.length;
-        }
-        return hash;
     }
 
     public void set(String key, int value) {
@@ -50,6 +29,9 @@ public class HashTable {
         } else {
             Node temp = dataMap[index];
             while (temp.next != null) {
+                if (temp.key.equals(key)) {
+                    temp.value = value;
+                }
                 temp = temp.next;
             }
             temp.next = newNode;
@@ -60,7 +42,7 @@ public class HashTable {
         int index = hash(key);
         Node temp = dataMap[index];
         while (temp != null) {
-            if (temp.key == key) {
+            if (temp.key.equals(key)) {
                 return temp.value;
             }
             temp = temp.next;
@@ -69,14 +51,24 @@ public class HashTable {
     }
 
     public ArrayList<String> keys() {
-        ArrayList<String> allKeys = new ArrayList<>();
+        ArrayList<String> keys = new ArrayList<>();
         for (int i = 0; i < dataMap.length; i++) {
             Node temp = dataMap[i];
             while (temp != null) {
-                allKeys.add(temp.key);
+                keys.add(temp.key);
                 temp = temp.next;
             }
         }
-        return allKeys;
+        return keys;
+    }
+
+    private int hash(String key) {
+        int hash = 0;
+        char[] keyChars = key.toCharArray();
+        for (int i = 0; i < keyChars.length; i++) {
+            int asciiValue = keyChars[i];
+            hash = (hash + asciiValue * 23) % dataMap.length;
+        }
+        return hash;
     }
 }
--- src/HashTable/main.java
+++ src/HashTable/main.java
@@ -1,5 +1,8 @@
 package HashTable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class main {
     public static void main(String[] args) {
         HashTable myHashTable = new HashTable();
@@ -11,6 +14,36 @@ public class main {
         myHashTable.set("bolts", 200);
         myHashTable.set("screws", 140);
 
-        myHashTable.printTable();
+        ArrayList<String> teste = myHashTable.keys();
+        System.out.println(teste);
+    }
+
+    public static boolean itemInCommon(int[] array1, int[] array2) {
+        HashMap<Integer, Boolean> myHashTable = new HashMap<>();
+        for (int number : array1) {
+            myHashTable.put(number, true);
+        }
+
+        for (int number : array2) {
+            if (myHashTable.containsKey(number))
+                return true;
+
+        }
+        return false;
+    }
+
+    public static ArrayList<Integer> findDuplicates(int[] collection) {
+        HashMap<Integer, Boolean> myHashMap = new HashMap<>();
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int number : collection) {
+            if (!myHashMap.containsKey(number)) {
+                myHashMap.put(number, false);
+            } else {
+                if (!result.contains(number))
+                    result.add(number);
+            }
+        }
+        return result;
     }
 }
